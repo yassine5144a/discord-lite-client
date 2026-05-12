cd1f4838e4d5ff71ebe7c9837abed0859cb4a1c5
yassine5144a <zgolmiyasso@gmail.com>
2026-05-12 22:48:25 +0200

    fix: add TURN servers for voice

--- src/components/VoiceChannel.js
+++ src/components/VoiceChannel.js
@@ -22,8 +22,24 @@ const RTC_CONFIG = {
   iceServers: [
     { urls: 'stun:stun.l.google.com:19302' },
     { urls: 'stun:stun1.l.google.com:19302' },
-    { urls: 'stun:stun2.l.google.com:19302' }
-  ]
+    // Free TURN servers for NAT traversal
+    {
+      urls: 'turn:openrelay.metered.ca:80',
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
+    },
+    {
+      urls: 'turn:openrelay.metered.ca:443',
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
+    },
+    {
+      urls: 'turn:openrelay.metered.ca:443?transport=tcp',
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
+    }
+  ],
+  iceCandidatePoolSize: 10
 };
 
 const AUDIO_CONSTRAINTS = {
